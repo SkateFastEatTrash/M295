cb4d9f4d4346cf65b2c50b2a27a62aa7d01a4c4a
Lucas Blom <51747171+SkateFastEatTrash@users.noreply.github.com>
2023-05-04 14:14:37 +0000

    Started on leding books and added Express-Session

--- bibliothek/server.js
+++ bibliothek/server.js
@@ -1,9 +1,14 @@
 // Defining Constants
+const app = express()
 const cors = require('cors');
 const express = require('express')
 const bodyParser = require('body-parser');
-const app = express()
+const session = require('express-session')
+
+//Specifying Port
 const port = 3300
+
+
 
 // --------------------------------------------------------------------------------------//
 app.use(cors());
@@ -18,31 +23,39 @@ app.listen(port, () => {
   );
 });
 
+
+// Bibliothek 1
+
 // List of Books
 var books = [
   {
     "title": "To Kill a Mockingbird",
     "author": "Harper Lee",
+    "lent": false,
     "ISBN": 9780446310789
   },
   {
     "title": "The Hitchhiker's Guide to the Galaxy",
     "author": "Douglas Adams",
+    "lent": false,
     "ISBN": 9780345391803
   },
   {
     "title": "The Da Vinci Code",
     "author": "Dan Brown",
+    "lent": false,
     "ISBN": 9780385504201
   },
   {
     "title": "Pride and Prejudice",
     "author": "Jane Austen",
+    "lent": true,
     "ISBN": 9780141439518
   },
   {
     "title": "One Hundred Years of Solitude",
     "author": "Gabriel García Márquez",
+    "lent": true,
     "ISBN": 9780060883287
   }
 ];
@@ -50,6 +63,12 @@ var books = [
 // returns JSON containing every Book
 function findAll() {
   return books;
+};
+
+// returns JSON containing every Book which is currently lent
+function findLends(){
+  const lends = books.filter((book) => book.lent == true)
+  return lends;
 };
 
 // returns the book which contains the given ISBN
@@ -63,6 +82,7 @@ function addnew(isbn, title, author) {
   const book = {
     "title": title,
     "author": author,
+    "lent": false,
     "ISBN": isbn
   };
   books.push(book);
@@ -75,12 +95,22 @@ function remove(isbn) {
 
 // changes information of given Book identified using ISBN
 // Also logs information about the updated book in the console
-function updated(isbn, title, author) {
+function updated(isbn, title, author, lent) {
   books.forEach(book => {
       if(book.ISBN == isbn){
           book.title = `${title}`;
           book.author =  `${author}`;
-          console.log(book.title  + '\n' + book.author + '\n' + book.ISBN + '\n');
+          book.lent = lent;
+          return(book.title  + '\n' + book.author + '\n' + book.ISBN + '\n' + book.lent + '\n');
+      }
+  });
+}
+
+function lendsBook(isbn, lent) {
+  books.forEach(book => {
+      if(book.ISBN == isbn){
+          book.lent = lent;
+          console.log(book.title  + '\n' + book.author + '\n' + book.ISBN + '\n' + book.lent + '\n');
       }
   });
 }
@@ -110,8 +140,9 @@ app.put('/books/:isbn', (req, res) => {
   const isbn = (req.params.isbn);
   const title = req.query.title;
   const author = req.query.author
+  const lent = req.query.lent
   console.log(title, author)
-  updated(isbn, title, author);
+  updated(isbn, title, author, lent);
   res.send(findAll());
   
 });
@@ -121,4 +152,23 @@ app.delete('/books/:isbn', (req, res) => {
   const isbn = req.params.isbn;
   remove(isbn);
   res.send(isbn + " has been deleted");
+});
+
+//--------------------------------------------------------------------------------------//
+
+// Bibliothek 2
+app.get('/lends', (req, res) => {
+  res.send(findLends())
+});
+
+app.get('/lends/:isbn', (req, res) => {
+  const isbn = (req.params.isbn);
+  res.send(findByID(isbn))
+});
+
+app.put('/lends/:isbn', (req, res) => {
+  const isbn = (req.params.isbn);
+  const lent = req.query.lent
+  lendsBook(isbn, lent);
+  res.send(findLends());
 });
